baab1a3728905b797ab39454c64f90747839598f
Hoanganh271120 <luonghoanganh2711@gmail.com>
2023-06-11 09:45:34 +0700

    create project front end demo

--- frontend-demo/src/App.js
+++ frontend-demo/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import UserForm from "./components/UserForm";
+
+class App extends Component {
+  state = {
+    repos: null
+  }
+  getUser = (e) => {
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    if (user) {
+      axios.get(`http://localhost:8888/user/`)
+      .then((res) => {
+        const repos = res.data;
+        this.setState({ repos });
+        // return res
+      })
+    } else return;
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">HTTP Calls in React</h1>
+        </header>
+        <UserForm getUser={this.getUser} />
+        { this.state.repos ? <p>Number of repos: { this.state.repos }</p> : <p>Please enter a username.</p> }
+      </div>
+    );
+  }
+};
 
 export default App;
